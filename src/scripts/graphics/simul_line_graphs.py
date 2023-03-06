bd303a0d8942d2eba1136a39bedb9dba45c88575
Jeronimo <jeronimopenha@gmail.com>
2023-03-05 22:25:54 -0300

    arrumando o projeto, corrigindo benchmarks, refazendo as chamadas de funções e criando os routers

--- src/scripts/graphics/simul_line_graphs.py
+++ src/scripts/graphics/simul_line_graphs.py
@@ -6,7 +6,7 @@ if os.getcwd() not in sys.path:
 
 import traceback
 import matplotlib.pyplot as plt
-from src.util import read_json_files, get_files
+from src.util import read_json_file, get_files
 
 
 if __name__ == '__main__':
@@ -17,13 +17,35 @@ if __name__ == '__main__':
         json_files = get_files(input_path, '.json')
 
         graph_dict = {}
-        simulations = {}
-        for file  in bench_files:
-            if bench not in simulations.keys():
-                simulations[bench] = {}
-        del bench, bench_files, bench_input_path
+        simul_results = {}
+        for file in json_files.keys():
+            bench = json_files[file]['file_name_no_ext'][:-4]
+            if bench not in simul_results.keys():
+                simul_results[bench] = []
+            simul_results[bench].append(json_files[file])
 
-        for 
+            del bench
+        del file
+
+        for bench in simul_results.keys():
+            simul_results[bench].sort(key=lambda v: v['file_name'])
+            throughput = []
+            for file in simul_results[bench]:
+                json_content = read_json_file(file['file_full_path'])
+                throughput.append(json_content['throughput_worst'])
+                del json_content
+            del file
+
+            plt.title('Results of simulations for %s' % bench)
+            x = [i+1 for i in range(len(simul_results[bench]))]
+            
+            plt.plot(x, throughput, label='throughput')
+            plt.legend()
+            plt.savefig(output_path_base + bench +'.png')
+            plt.cla()
+            plt.clf()
+            a = 1
+        del bench
 
         '''for strategy in strategies:
 
